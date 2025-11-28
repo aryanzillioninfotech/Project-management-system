94b1aa8a75bf663bbcaa41588e06b2bf5bcf0ca3
aryanzillioninfotech <aryan.zillioninfotech@gmail.com>
2025-11-28 18:00:30 +0530

    changes ...

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -3,14 +3,14 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-    api = 'http://localhost:3000';
-    constructor(private http: HttpClient) { }
-    get<T>(url: string, params?: any) {
-        return this.http.get<T>(`${this.api}/${url}`, { params });
-    }
+  api = 'http://localhost:3000';
+  constructor(private http: HttpClient) { }
+  get<T>(url: string, params?: any) {
+    console.log("CALLING URL:", `${this.api}/${url}`);
+    return this.http.get<T>(`${this.api}/${url}`, { params });
+  }
 
-
-    post<T>(endpoint: string, body: any) {
+  post<T>(endpoint: string, body: any) {
     return this.http.post<T>(`${this.api}/${endpoint}`, body);
   }
 
--- src/app/tasks/task-list/task-list.ts
+++ src/app/tasks/task-list/task-list.ts
@@ -8,22 +8,41 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <h3>All Tasks</h3>
-    <ul class="list-group">
+    <ul class="list-group" *ngIf="tasks.length > 0; else noData">
       <li *ngFor="let t of tasks" class="list-group-item d-flex justify-content-between">
-        <div>{{t.title}} <small class="text-muted">({{t.status}})</small></div>
+        <div>{{ t.name }} <small class="text-muted">({{ t.state }})</small></div>
         <div>
           <button class="btn btn-sm btn-outline-primary" (click)="toggle(t)">Toggle</button>
         </div>
       </li>
     </ul>
+
+    <ng-template #noData>
+      <p class="text-danger">No Tasks Found</p>
+    </ng-template>
   `
 })
 export class TaskListComponent implements OnInit {
   tasks: any[] = [];
+
   constructor(private api: ApiService) {}
-  ngOnInit() { this.load(); }
-  load() { this.api.get('tasks').subscribe((r:any)=> this.tasks = r); }
-  toggle(t:any) { const next = t.status === 'todo' ? 'in-progress' : (t.status === 'in-progress' ? 'done' : 'todo');
-    this.api.put(`tasks/${t.id}`, {...t, status: next}).subscribe(()=>this.load());
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.api.get('tasks').subscribe((r: any) => {
+      console.log("Loaded Tasks:", r);
+      this.tasks = r;
+    });
+  }
+
+  toggle(t: any) {
+    const next = t.state === 'todo'
+      ? 'in-progress'
+      : (t.state === 'in-progress' ? 'done' : 'todo');
+
+    this.api.put(`tasks/${t.id}`, { ...t, state: next }).subscribe(() => this.load());
   }
 }
